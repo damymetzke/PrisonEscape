049494d46929c655bfd05633a9df16228d02bae0
Damy Metzke <damy.metzke@gmail.com>
2020-11-15 21:43:08 +0100

    Add function te select prisoner.

--- Source/CSharp/Autoload/PrisonerSelectionManager.cs
+++ Source/CSharp/Autoload/PrisonerSelectionManager.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class PrisonerSelectionManager : Node
 {
     private List<Prisoner> Prisoners = new List<Prisoner>();
+    private List<Prisoner> SelectedPrisoners = new List<Prisoner>();
 
     public PrisonerSelectionManager()
     {
@@ -19,5 +20,26 @@ public class PrisonerSelectionManager : Node
     internal void DeRegisterPrisoner(Prisoner value)
     {
         Prisoners.Remove(value);
+    }
+
+    internal void SelectSinglePrisoner(Prisoner value)
+    {
+        foreach (Prisoner prisoner in SelectedPrisoners)
+        {
+            if (prisoner == value)
+            {
+                continue;
+            }
+            // TODO: notify prisoner it has been deselected.
+        }
+        SelectedPrisoners.Clear();
+
+        if (SelectedPrisoners.Contains(value))
+        {
+            return;
+        }
+
+        // TODO: notify prisoner it has been seleced.
+        SelectedPrisoners.Add(value);
     }
 }
